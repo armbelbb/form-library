96e969373ec562da74ff5a5d5f80388f72d25347
Armbel Bernal <abernal@ondemand-innovation.com>
2023-05-25 23:22:26 +0800

    db_connection setting for hostinger

--- db_connection.php
+++ db_connection.php
@@ -4,4 +4,10 @@
 	$pword = "";
 	$dbname = "form_vault";
 	$conn = new mysqli($server, $uname, $pword, $dbname);
+
+	// $server = "localhost";
+	// $uname = "u119951004_sst";
+	// $pword = "Sst!1234";
+	// $dbname = "u119951004_formvault";
+	// $conn = new mysqli($server, $uname, $pword, $dbname);
 ?>
