aa93dfee7fe566e93a81bdf750f0d3eebdf83307
Armbel Bernal <ondemand-innovation.com>
2023-05-27 17:44:15 +0800

    updates 5.27.23 - forms. buttons

--- forms-request.php
+++ forms-request.php
@@ -89,7 +89,7 @@
                                                         echo "<td>$request[reference_id]</td>";
                                                     echo "<td>$request[display_name]</td>";
                                                     echo "<td>" . date('F d, Y  g:i:A', strtotime($request['request_date'])) . "</td>";
-                                                    echo "<td>";
+                                                    echo "<td class='text-right'>";
                                                         echo "<button class='btn btn-primary' onclick='$(\"#viewRequestModal$request[form_request_id]\").modal(\"toggle\")'>VIEW</button>&nbsp";
                                                         echo "<a class='btn btn-success' onclick='$(\"#completeRequestModal$request[form_request_id]\").modal(\"toggle\")'>";
                                                             // echo "<i class='fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400'></i>";
