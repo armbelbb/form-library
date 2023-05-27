6b52a36c980fee09b3236e757c83fa7c04022227
Armbel Bernal <ondemand-innovation.com>
2023-05-27 22:47:38 +0800

    nav-bar notification

--- _navbar.php
+++ _navbar.php
@@ -13,10 +13,22 @@
                 <i class="fas fa-bell"></i>
                 &nbsp;Notification
             </a>
+            <!-- Notification Dropdown -->
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownNotification">
+                <!-- Dropdown header -->
+                <h6 class="dropdown-header">Notifications</h6>
+                <!-- Dropdown items -->
+                <a class="dropdown-item" href="#">Notification 1</a>
+                <a class="dropdown-item" href="#">Notification 2</a>
+                <a class="dropdown-item" href="#">Notification 3</a>
+                <!-- Dropdown footer -->
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">View All Notifications</a>
+            </div>
         </li>
-    
+
         <div class="topbar-divider d-none d-sm-block"></div>
-    
+
         <!-- Nav Item - User Information -->
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
@@ -25,28 +37,7 @@
                 <img class="img-profile rounded-circle"
                     src="img/undraw_profile.svg">
             </a>
-            <!-- Dropdown - User Information -->
-            <!-- <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                aria-labelledby="userDropdown"> -->
-                <!-- <a class="dropdown-item" href="#">
-                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Profile
-                </a>
-                <a class="dropdown-item" href="#">
-                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Settings
-                </a>
-                <a class="dropdown-item" href="#">
-                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Activity Log
-                </a> -->
-                <!-- <div class="dropdown-divider"></div> -->
-                <!-- <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Logout
-                </a> -->
-            <!-- </div> -->
         </li>
     </ul>
-    
+
 </nav>
