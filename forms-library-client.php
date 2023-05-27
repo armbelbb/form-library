d6afb495a726f11bcc33de3cc4033394cf00a164
Armbel Bernal <ondemand-innovation.com>
2023-05-27 11:30:16 +0800

    ---

--- forms-library-client.php
+++ forms-library-client.php
@@ -94,7 +94,7 @@
                                                             echo "<td class='align-middle'>$form[form_name]</td>";
                                                             echo "<td class='align-middle'>$form[reference_id]</td>";
                                                             echo "<td class='text-right'>";
-                                                                echo "<button class='btn btn-outline-primary' onclick='$(\"#viewFormModal$form[id]\").modal(\"toggle\")'>View Form</button>&nbsp";
+                                                                echo "<button class='btn btn-primary' onclick='$(\"#viewFormModal$form[id]\").modal(\"toggle\")'>View Form</button>&nbsp";
                                                                 // echo "<button class='btn btn-outline-success' onclick='$(\"#viewWorkflow$form[id]\").modal(\"toggle\")'>View Workflow</button>";
                                                             echo "</td>";
                                                         echo "</tr>";
